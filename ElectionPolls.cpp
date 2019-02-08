9727013ef59fbc1163379d248315f3214a5de38c
Abhishek Patel <47433467+abhishekpatelece@users.noreply.github.com>
2019-02-09 00:53:10 +0530

    Update ElectionPolls.cpp

--- ElectionPolls.cpp
+++ ElectionPolls.cpp
@@ -1,16 +1,15 @@
+//consider candidates are not participating in voting.
 #include<iostream>
 #include<string>
 using namespace std;
 int main()
 {
-//consider candidates are not participating in voting.
 int v,c;
 cout<<"Enter the number of all voters: ";
 cin>>v;
 cout<<"Enter the number of Candidate: ";
 cin>>c;
-//declaring an array for counting votes
-int *cv=new int[c];
+int *cv=new int[c];//declaring an array for counting votes
 for(int j;j<c;j++)
     cv[j]=0;
 string cand[c];
@@ -21,15 +20,14 @@ cout<<"\n\nVote anybody from below list"<<endl;
 cout<<"__________________________________________________"<<endl;
 for(int i=0;i<c;i++)
     cout<<cand[i]<<endl;
+
 //to vote enter the names any from candidate
+    
 cout<<"Enter the name of candidates "<<v<<" times randomly to vote them: "<<endl;
 string vote[v];
 for(int i=0;i<v;i++)
 {
     cin>>vote[i];
-}
-for(int i=0;i<v;i++)
-{
     for(int j=0;j<c;j++)
     {
         if(vote[i]==cand[j])
@@ -42,7 +40,9 @@ for(int i=0;i<c;i++)
     if(cv[i]>max)
         {max=cv[i];temp.erase();temp=cand[i];}
     }
+    
 //arrange candidate in alphabetically who got the same votes
+    
 for(int i=0;i<c;i++)
 {
     if(max==cv[i]&& temp!=cand[i])
